78f0ebbdbeb3a5168d9c3b5ff2fc3978046f5750
BurNing1993 <786678257@qq.com>
2020-08-04 17:57:48 +0800

    feat:up base

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
   someSidebar: {
-    JavaScript: ['js-built-in-types', 'js-equality-comparisons-and-sameness','js-prototype']
+    JavaScript: ['js-built-in-types', 'js-equality-comparisons-and-sameness', 'js-prototype'],
+    Base: ['base-html', 'base-css', 'base-js', 'base-computer-operating-system', 'base-computer-network', 'base-puzzle', 'base-algorithm', 'base-interview-questions', 'base-tool'],
   },
 };
