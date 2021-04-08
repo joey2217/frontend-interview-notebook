494c2e1974652d3304740197c3d7e3136c420844
Joey <786678257@qq.com>
2021-04-08 17:08:43 +0800

    feat:interview react

--- blog/code/throttle.js
+++ blog/code/throttle.js
@@ -1,28 +1,19 @@
-/**
- * @param {Function} fn 
- * @param {Number} delay 
- */
-function throttle(fn, delay = 500) {
-  let timer = null;
-  let previous = Date.now();
-  return function (...args) {
-    const now = Date.now();
-    const remaining = delay - (now - previous); //距离规定时间,还剩多少时间
-    clearTimeout(timer);  //清除之前设置的定时器
-    if (remaining <= 0) {
-      fn.apply(this, args);
-      previous = Date.now();
-    } else {
-      timer = setTimeout(() => {
-        fn.apply(this, args)
-      }, remaining); //因为上面添加的clearTimeout.实际这个定时器只有最后一次才会执行
-    }
-  }
+function throttle(fn, delay) {
+    var _this = this;
+    if (delay === void 0) { delay = 500; }
+    var canRun = true;
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        if (!canRun) {
+            return;
+        }
+        canRun = false;
+        setTimeout(function () {
+            fn.apply(_this, args);
+            canRun = true;
+        }, delay);
+    };
 }
-function log(s) {
-  console.log(s);
-}
-var throttledLog = throttle(log, 2000);
-window.addEventListener('scroll', function (e) {
-  throttledLog(e);
-});
---
+++ blog/leetcode/longestCommonPrefix.ts
@@ -0,0 +1,8 @@
+function longestCommonPrefix(strs: string[]): string {
+  let pre =""
+  let temp = strs[0]
+  strs.forEach(str=>{
+    let temp = 
+  })
+  return pre;
+};
